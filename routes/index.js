8a7bfe4baa1ce80cc4bf4c385a3736b6804016c0
luohao <496952252@qq.com>
2016-11-14 17:28:52 +0800

    a

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,21 @@ router.get('/', function(req, res, next) {
 
 router.get('/test',function(req,res){
 
-    http.get("http://www.biquge.com/13_13453/7436033.html", function(ress) {
+    var options={
+        host:"www.biquge.com",
+        path:"/13_13453/7436033.html",
+        headers: {
+            "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept-Encoding":"gzip, deflate, sdch, br",
+            "Accept-Language":"zh-CN,zh;q=0.8",
+            "Cache-Control":"max-age=0",
+            "Connection":"keep-alive",
+            "Upgrade-Insecure-Requests":1,
+            "User-Agent:":"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36"
+        }
+    }
+
+    http.get(options, function(ress) {
        var data=""
         ress.on('data', function (chunk) {
            data+=chunk;
