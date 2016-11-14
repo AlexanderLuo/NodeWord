30385538f10d281069d7487a3739050f28cb926d
luohao <496952252@qq.com>
2016-11-14 18:15:43 +0800

    a

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 
 var express = require('express');
 var router = express.Router();
-var http = require('http');
+var request = require('request');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,34 +12,13 @@ router.get('/', function(req, res, next) {
 
 router.get('/test',function(req,res){
 
-    var options={
-        host:"www.biquge.com",
-        path:"/13_13453/7436033.html",
-        //headers: {
-        //    "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-        //    "Accept-Encoding":"gzip, deflate, sdch, br",
-        //    "Accept-Language":"zh-CN,zh;q=0.8",
-        //    "Cache-Control":"max-age=0",
-        //    "Connection":"keep-alive",
-        //    "Upgrade-Insecure-Requests":1,
-        //    "User-Agent:":"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36"
-        //}
-    }
-
-    http.get(options, function(ress) {
-       var data=""
-        ress.on('data', function (chunk) {
-           data+=chunk;
-        });
-        ress.on('end', function(){
-
-            var a=data.match(/<div id=\"content\">([\s\S]+?(<div[\s\S]+?<\/div>)*?[\s\S]+?)*?<\/div>/)
-
+    request('http://www.biquge.com/13_13453/7436033.html', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var a=body.match(/<div id=\"content\">([\s\S]+?(<div[\s\S]+?<\/div>)*?[\s\S]+?)*?<\/div>/)
             res.send(a[0])
-        });
-    }).on('error', function(e) {
-        console.log("Got error: " + e.message);
-    });
+
+        }
+    })
 
 
 
