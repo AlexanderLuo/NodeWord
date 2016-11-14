868099e8f497d783a77d5213f29917893be88c66
luohao <496952252@qq.com>
2016-11-14 16:52:48 +0800

    a

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,33 @@
+
 var express = require('express');
 var router = express.Router();
+var http = require('http');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
 router.get('/test',function(req,res){
-    res.send("wowo")
+
+    http.get("http://www.biquge.com/13_13453/7436033.html", function(ress) {
+       var data=""
+        ress.on('data', function (chunk) {
+           data+=chunk;
+        });
+        ress.on('end', function(){
+
+            var a=data.match(/<div id=\"content\">([\s\S]+?(<div[\s\S]+?<\/div>)*?[\s\S]+?)*?<\/div>/)
+
+            res.send(a[0])
+        });
+    }).on('error', function(e) {
+        console.log("Got error: " + e.message);
+    });
+
+
+
 })
 
 
